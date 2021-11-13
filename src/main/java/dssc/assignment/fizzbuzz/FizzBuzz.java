6e3a3fc33870e194997f3403f7222baed5258707
Isacco <isacco.zinna@gmail.com>
2021-11-13 16:44:15 +0100

    FizzBuzzBang code added

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,21 @@
 package dssc.assignment.fizzbuzz;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+        Stream.iterate(1, n -> n+1)
+                .limit(105)
+                .map(FizzBuzz::fizzBuzzBang)
+                .forEach(System.out::println);
+    }
+
+    public static String fizzBuzzBang(int in){
+        String result = Optional.of(in)
+                .map(n -> ((n%3 == 0) ? "Fizz" : "") + ((n%5 == 0) ? "Buzz" : "") + ((n%7 == 0) ? "Bang" : "")).get();
+        return result.isEmpty() ? Integer.toString(in) : result;
     }
 
 }
